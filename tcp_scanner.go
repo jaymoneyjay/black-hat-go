0403b4d65a1fdb02e4d7ed5172eb313840f14aa4
Jodok Vieli <jvieli@student.ethz.ch>
2021-09-28 16:16:55 +0200

    open ports ordered

--- tcp_scanner.go
+++ tcp_scanner.go
@@ -2,28 +2,54 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"net"
-	"sync"
 )
 
-func main() {
-	var wg sync.WaitGroup
-	for i:=0; i<=65535;i++ {
-		wg.Add(1)
-		go func(port int) {
-			defer wg.Done()
-			address := fmt.Sprintf("127.0.0.1:%d", port)
-			con, err := net.Dial("tcp", address)
-			if err != nil {
-				return
-			}
-			statement := fmt.Sprintf("Port %d open.", port)
-			fmt.Println(statement)
-			con.Close()
-
-		}(i)
+func worker(ports, results chan int, target string) {
+	for p := range ports {
+		address := fmt.Sprintf("%s:%d", target, p)
+		conn, err := net.Dial("tcp", address)
+		if err != nil {
+			results <- 0
+			continue
+		}
+		conn.Close()
+		results <- p
 	}
-	wg.Wait()
+}
+
+func main() {
+	TAR := "127.0.0.1"
+	MAXPORT := 1024
+	ports := make(chan int, 100)
+	results := make(chan int)
+	var openports []int
+
+	for i:=0; i<=cap(ports); i++ {
+		go worker(ports, results, TAR)
+	}
+
+	go func() {
+		for i:=1; i<=MAXPORT; i++ {
+			ports <- i
+		}
+	}()
+
+	for i:=0; i<MAXPORT; i++ {
+		port := <-results
+		if port != 0 {
+			openports = append(openports, port)
+		}
+	}
+
+	close(ports)
+	close(results)
+
+	sort.Ints(openports)
+	for _, port := range openports {
+		fmt.Printf("%d open\n", port)
+	}
 }
 
 
