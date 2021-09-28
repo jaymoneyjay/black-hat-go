4c45f0ec0a463c1a1f52ded50ae818676fde1759
Jodok Vieli <jvieli@student.ethz.ch>
2021-09-28 15:35:57 +0200

    concurrent scanning

--- tcp_scanner.go
+++ tcp_scanner.go
@@ -3,23 +3,27 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
-func scanPort(port int) {
-	address := fmt.Sprintf("scanme.nmap.org:%d", port)
-	con, err := net.Dial("tcp", address)
-	if err != nil {
-		return
-	}
-	statement := fmt.Sprintf("Port %d open.", port)
-	fmt.Println(statement)
-	con.Close()
-}
-
 func main() {
-	for i:=1; i<=1024; i++ {
-		scanPort(i)
+	var wg sync.WaitGroup
+	for i:=0; i<=65535;i++ {
+		wg.Add(1)
+		go func(port int) {
+			defer wg.Done()
+			address := fmt.Sprintf("127.0.0.1:%d", port)
+			con, err := net.Dial("tcp", address)
+			if err != nil {
+				return
+			}
+			statement := fmt.Sprintf("Port %d open.", port)
+			fmt.Println(statement)
+			con.Close()
+
+		}(i)
 	}
+	wg.Wait()
 }
 
 
