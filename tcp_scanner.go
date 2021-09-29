c90f42f3ca78ffa588610037f6dbf2b63537c69f
Jodok Vieli <jvieli@student.ethz.ch>
2021-09-29 10:45:34 +0200

    proxy server

--- tcp_scanner.go
+++ tcp_scanner.go
@@ -15,7 +15,47 @@ func main() {
 	maxPort := 1024
 	numWorkers := 100
 	scanTCP(target, maxPort, numWorkers)
-	serve()
+	startProxy()
+}
+
+////
+// TCP POXY
+////
+
+// runs the proxy server
+func startProxy() {
+	listener, err := net.Listen("tcp", "0.0.0.0:20080")
+	if err != nil {
+		log.Fatalln("Unable to bind to port")
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatalln("Unable to accept connection")
+		}
+		go handle(conn)
+	}
+}
+
+// forwards all traffic to port 80 to google.com
+func handle(src net.Conn) {
+	dst, err := net.Dial("tcp", "google.com:80")
+	if err != nil {
+		log.Fatalln("Unable to connect")
+	}
+	defer dst.Close()
+
+	go func() {
+		if _, err = io.Copy(dst, src); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	if _, err = io.Copy(src, dst); err != nil {
+		log.Fatalln(err)
+	}
+
 }
 
 ////
